65ac40c6d1a621993b1b94b974805eafe535e137
Anoop072001 <anoopsidhan777@gmail.com>
2022-01-29 20:08:49 +0530

    comments added

--- speech_text.py
+++ speech_text.py
@@ -1,6 +1,9 @@
 import speech_recognition as sr
 
+# r will be used as a recognizer for forward referencing
 r = sr.Recognizer()
+
+# Now we provide a audio for converting to text
 with sr.AudioFile("Hello.wav") as source:
     audio = r.listen(source)
 
